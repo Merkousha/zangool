c00dfdeb7d0fa71bce47069a07f9271856b79112
Merko <Merko@LAPTOP-3P627PH6>
2023-06-06 19:34:20 +0330

    first commit

---
+++ members/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def members(request):
+    return HttpResponse("<h1>Hello World</h1><br/><a href='https://devmap.ir'>devmap.ir</a>")
