408daad16ffc0dec860fd4c4e3d35eb7640ddf4b
abolfazl-shekarpour <Abolfazlshekarpour123@gmail.com>
2023-07-11 01:19:42 +0400

    i fixed about

--- members/urls.py
+++ members/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('members/', views.members, name='members'),
     path('index/', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
 ]
-
--- members/views.py
+++ members/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
 def members(request):
     return HttpResponse("<h1>Hello World</h1><br/><a href='https://devmap.ir'>devmap.ir</a>")
+
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
 
-    
+def home(request):
+    return render(request, 'home.html')
 
+def about(request):
+    return render(request, 'about.html')
