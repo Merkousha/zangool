0e78e0e752385f7419a1eb92f3064c46df2aef7b
Merko <Merko@LAPTOP-3P627PH6>
2023-08-26 19:51:22 +0330

    Create Event model and append it to Admin panel

--- members/admin.py
+++ members/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from members.models import Event
 
 # Register your models here.
+
+
+
+admin.site.register(Event)
--- members/models.py
+++ members/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    address = models.CharField(max_length=300)
+    time = models.DateTimeField(auto_now_add=True)
+    picture = models.ImageField(null=True,blank=True,)#upload_to=get_file_path
+    
